948f786d1edf2c2097c62641042aa8b81aa3e226
developerdtodo-netizen <developerdtodo@gmail.com>
2026-04-07 11:34:17 -0500

    feat: preparar el provider de developers

--- lib/presentation/providers/03_developers/developers_provider.dart
+++ lib/presentation/providers/03_developers/developers_provider.dart
@@ -3,3 +3,24 @@ import 'package:uuid/uuid.dart';
 enum DevsFilter { all, attended, absentee }
 
 const uuid = Uuid();
+
+
+//================== Manejo de los filtros ======================
+
+
+
+
+
+
+
+//================== Lista de desarrolladores ======================
+
+
+
+
+
+
+
+
+
+//================== Lista + Filtros ======================
